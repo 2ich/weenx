7bae92d25bb8a8dc2e864110c0dbfcf9c492c05d
2ich <exple@example.io>
2022-02-23 15:16:59 +0300

    server-side cells but bugs

--- index.js
+++ index.js
@@ -16,18 +16,91 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+function arrayRemove(arr, value) { 
+
+    return arr.filter(function(ele){ 
+        return ele != value; 
+    });
+}
+
+rooms = []
+
+rooms.defu = {
+    users: [],
+    cells: []
+}
+
+cwidth = 900
+cheight = 600
+// canvas.width = cwidth
+// canvas.height = cheight
+
+gz = 50
+xcells = cwidth / gz
+ycells = cheight / gz
+
+console.log(xcells, ycells)
+
+// cl_bg = "#0088ff"
+cl_bg = "#fff"
+cl_empty = '#f8f8f8'
+cl_self = '#141414'
+cl_selc = 'orange'
+
+selc = {
+    x: 1000,
+    y: 1000
+} 
+
+sells = []
+
+for (let i = 0; i < ycells; i++) {
+    selly = []
+    for (let j = 0; j < xcells; j++) {
+        selly.push({
+            color: cl_empty,
+            selected: false,
+        })
+    }
+    sells.push(selly)
+}
+
+// cells[3][5].color = 'orange'
+sells[2][7].color = 'crimson'
+
+rooms.defu.cells = sells
+
 
 io.on('connection', (socket) => {
     console.log('new connection', socket.id)
 
+    rooms.defu.users.push(socket.id)
+    console.log(rooms)
+
     socket.on('disconnect', () => {
         console.log(socket.id, 'disconnected')
+        
+        rooms.defu.users = arrayRemove(rooms.defu.users, socket.id)
     })
 
     socket.on('move', (data) => {
         socket.broadcast.emit('move', data)
         console.log(data)
     })
+
+    socket.on('new-player', (cb) => {
+        // socket.broadcast.emit({
+        //     cells: rooms.defu.cells
+        // })
+        cb({
+            cells: rooms.defu.cells
+        })
+    })
+    // socket.on('new', (data) => {
+    //     // console.log(data)
+    //     if (!rooms.defu.cells)        
+    //         rooms.defu.cells = data.cells
+    // })
 })
 
 
